5ed03e4faf86410d602e59a6ab7bacea5e8d7714
elijah obara <elijahobara357@gmail.com>
2023-02-23 09:37:23 +0300

    Configured log out and styled my app

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FaUser } from 'react-icons/fa';
-
-
+import Modal from './Modal';
 export default function Header() {
-  return (
+  const [openModal, setOpenModal] = useState(false)
+
+
+
+
+  return(
+    <>
+    {openModal && <Modal setOpenModal={setOpenModal}/>}
     <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white'>
     <h1 className='text-3xl select-none sm:text-6xl'>TODO LIST</h1>
-        <FaUser className='text-xl sm:text-3xl duration-300 hover:opacity-40 cursor-pointer'/>
+        <FaUser onClick={() => setOpenModal(true)} className='text-xl sm:text-3xl duration-300 hover:opacity-40 cursor-pointer'/>
     </div>
-  )
+    </>
+    )
 }
-  
+       
--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
             </Head>
             <body>
                 <Main />
